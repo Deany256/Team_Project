09b8a2641184be6ac5fc5b582ad08315b958f84d
Deany256 <gdeandude@gmail.com>
2023-04-28 11:48:52 +0100

    Home method functionality working

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ def login():
 def signup():
     if request.method == "POST":
         if request.form.get("signup"):
-            name = request.form["new_username"]
+            name = request.form["name"]
+            username = request.form["new_username"]
             password = request.form["new_password"].encode("utf-8")
             # hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
             hashed_password = hashlib.sha256(password).hexdigest()
@@ -96,12 +97,12 @@ def signup():
 
             with sqlite3.connect(DATABASE) as conn:
                 cursor = conn.cursor()
-                cursor.execute("SELECT * FROM Customers WHERE name = ?", (name,))
+                cursor.execute("SELECT * FROM Customers WHERE name = ?", (username,))
                 result = cursor.fetchone()
                 if result is not None:
                     return "Username already exists, please choose a different username."
                 else:
-                    return "Invalid username or password"
+                    cursor.execute("INSERT into Customers (name, username, email, password_hash, shipping_address) VALUES (?, ?, ?, ?, ?)", (name, username, email, hashed_password,shipping_address))
     else:
         if "username" in session:
             return redirect("/home")
@@ -112,9 +113,10 @@ def signup():
 @app.route("/home")
 def home():
     if "username" in session:
-        return "Hello, " + session["username"]
+        # return "Hello, " + session["username"]
+        return render_template("home.html", username = session["username"])
     else:
-        return redirect("/hide")
+        return redirect("/login_or_signup")
 
 # Logout route
 @app.route("/logout")
